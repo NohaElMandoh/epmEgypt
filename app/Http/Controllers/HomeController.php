4126dfe3e03d8beadea1fb50f6830904a6fa014d
noha_m_elmandoh@outlook.com <noha_m_elmandoh@outlook.com>
2021-07-31 15:51:30 +0200

    update search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,17 +66,17 @@ class HomeController extends Controller
     }
     public function news(Request $request)
     {
-       
+
         if ($request->has('name'))
-        $news = News::where('title_en', 'like', '%' . $request->name . '%')->orWhere('title_ar', 'like', '%' . $request->name . '%')->get();
-        elseif($request->has('cat_id') )
-        $news = News::where('category_id', $request->cat_id)->get();
-        elseif($request->has('search_month') && $request->has('search_year') )
-        $news = News::whereYear('created_at', '=', $request->search_year)
-        ->whereMonth('created_at', '=',  $request->search_month)
-        ->get();
+            $news = News::where('title_en', 'like', '%' . $request->name . '%')->orWhere('title_ar', 'like', '%' . $request->name . '%')->get();
+        elseif ($request->has('cat_id'))
+            $news = News::where('category_id', $request->cat_id)->get();
+        elseif ($request->has('search_month') && $request->has('search_year'))
+            $news = News::whereYear('created_at', '=', $request->search_year)
+                ->whereMonth('created_at', '=',  $request->search_month)
+                ->get();
         else
-        $news = News::get();
+            $news = News::get();
         $last_news = News::latest()->take(3)->get();
         $comments = Comment::latest()->take(5)->get();
         return view('front.news', compact('news', 'last_news', 'comments'));
@@ -102,10 +102,7 @@ class HomeController extends Controller
         //     return response()->json(['message' => 'Error!']);
         // }
     }
-    public function search(Request $request)
-    {
-        $items = News::where('title_en', 'like', '%' . $request->name . '%')->orWhere('title_ar', 'like', '%' . $request->name . '%')->get();
-    }
+    
     public function facility()
     {
         $facilities = Facility::get();
--- resources/views/front/partials/navigation.blade.php
+++ resources/views/front/partials/navigation.blade.php
@@ -10,14 +10,14 @@
                 <div class="nav-inner">
                     <!-- BEGIN NAV -->
                     <ul id="nav" class="hidden-xs">
-                        <li class="drop-menu"> <a href="{{route('home')}}" class="level-top active"><span>{{__('home.Home')}}</span></a></li>
-                        <li class="drop-menu"> <a class="level-top" href="{{route('aboutUs')}}"><span>{{__('home.About us')}}</span> </a></li>
-                        <li class="mega-menu"> <a class="level-top" href="{{route('partners')}}"><span>{{__('home.Our Partners')}}</span></a></li>
-                        <li class="mega-menu"> <a class="level-top" href="{{route('news')}}"><span>{{__('home.News')}}</span></a></li>
-                        <li class="mega-menu"> <a class="level-top" href="{{route('facility')}}"><span>{{__('home.Facilities')}}</span></a></li>
-                        <li class="mega-menu"> <a class="level-top" href="{{route('courses')}}"><span>{{__('home.Training Courses')}}</span></a></li>
-                        <li class="mega-menu"> <a class="level-top" href="{{route('contactUs')}}"><span>{{__('home.Contact Us')}}</span></a> </li>
-                        <li class="drop-menu"> <a class="level-top" href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}"><span>{{__('home.English')}}</span> </a>
+                        <li class="drop-menu"> <a href="{{route('home')}}" class="level-top {{ (Route::currentRouteName()== 'home' ) ? 'active' : '' }} " href="{{route('home')}}"><span>{{__('home.Home')}}</span></a></li>
+                        <li class="drop-menu"> <a class="level-top {{ (Route::currentRouteName()== 'aboutUs' ) ? 'active' : '' }} "href="{{route('aboutUs')}}"><span>{{__('home.About us')}}</span> </a></li>
+                        <li class="mega-menu"> <a class="level-top {{ (Route::currentRouteName()== 'partners' ) ? 'active' : '' }}" href="{{route('partners')}}"><span>{{__('home.Our Partners')}}</span></a></li>
+                        <li class="mega-menu"> <a class="level-top  {{ (Route::currentRouteName()== 'news' ) ? 'active' : '' }}" href="{{route('news')}}"><span>{{__('home.News')}}</span></a></li>
+                        <li class="mega-menu"> <a class="level-top {{ (Route::currentRouteName()== 'facility' ) ? 'active' : '' }}" href="{{route('facility')}}"><span>{{__('home.Facilities')}}</span></a></li>
+                        <li class="mega-menu"> <a class="level-top {{ (Route::currentRouteName()== 'courses' ) ? 'active' : '' }}" href="{{route('courses')}}"><span>{{__('home.Training Courses')}}</span></a></li>
+                        <li class="mega-menu"> <a class="level-top {{ (Route::currentRouteName()== 'contactUs' ) ? 'active' : '' }}" href="{{route('contactUs')}}"><span>{{__('home.Contact Us')}}</span></a> </li>
+                        <li class="drop-menu"> <a class="level-top " href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}"><span>{{__('home.English')}}</span> </a>
                             <ul>
 
                                 <li> <a href="{{ LaravelLocalization::getLocalizedURL('ar', null, [], true) }}"> <span>{{__('home.Arabic')}} </span> </a> </li>
